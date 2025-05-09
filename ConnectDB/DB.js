a1f9b3d767f5fcd44fe350f3cfec21f3dc370758
moshaosama <mohamedOSFekry@gmail.com>
2025-05-09 23:59:37 +0300

    Latest Version

--- ConnectDB/DB.js
+++ ConnectDB/DB.js
@@ -1,12 +1,14 @@
 import mysql from "mysql2";
+import dotenv from 'dotenv'
 
+dotenv.config({config: ".env"});
 
- const DB = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "candidator"
-})
-
+const DB = mysql.createConnection({
+    host: process.env.MYSQLHOST || "localhost",
+    user: process.env.MYSQLUSER || "root",
+    password: process.env.MYSQL_ROOT_PASSWORD || "",
+    database: process.env.MYSQL_DATABASE || "candidator",
+    port: process.env.MYSQLPORT || 3306
+});
 
 export default DB;
--- index.js
+++ index.js
@@ -4,11 +4,21 @@ import DB from "./ConnectDB/DB.js"
 import createRecruiterRouter from "./Router/Recruiter/createRecruiter.js"
 import getRecruiterRouter from "./Router/Recruiter/getRecruiter.js"
 import cors from "cors"
-dotenv.config({config: "./Config/.env"});
+dotenv.config({config: ".env"});
 
 const app = express();
-const Port = process.env.PORT || 8000;
+const Port = process.env.PORT;
 
+
+setInterval(() => {
+    DB.query("SELECT 1", (err) => {
+        if (err) {
+            console.error("MySQL Keep-Alive Failed:", err.message);
+          } else {
+            console.log("MySQL Keep-Alive Ping sent");
+          }
+    })
+}, 5 * 60 * 1000)
 
 
 //Middleware
