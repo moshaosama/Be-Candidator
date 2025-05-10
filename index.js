2e06bd91d9b29640625218b2130fddfa2b292d07
moshaosama <mohamedOSFekry@gmail.com>
2025-05-10 23:29:38 +0300

    Update main index.js file

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
-import express from "express"
-import dotenv from "dotenv"
-import DB from "./ConnectDB/DB.js"
-import createRecruiterRouter from "./Router/Recruiter/createRecruiter.js"
-import getRecruiterRouter from "./Router/Recruiter/getRecruiter.js"
-import cors from "cors"
-dotenv.config({config: ".env"});
+import express from "express";
+import dotenv from "dotenv";
+import DB from "./ConnectDB/DB.js";
+import createRecruiterRouter from "./Router/Recruiter/createRecruiter.js";
+import getRecruiterRouter from "./Router/Recruiter/getRecruiter.js";
+import cors from "cors";
+import {
+  deleteAllRecruiterRouter,
+  deleteRecruiterByIdRouter,
+} from "./Router/Recruiter/deleteRecruiter.js";
+dotenv.config({ config: ".env" });
 
 const app = express();
 const Port = process.env.PORT;
 
-
 setInterval(() => {
-    DB.query("SELECT 1", (err) => {
-        if (err) {
-            console.error("MySQL Keep-Alive Failed:", err.message);
-          } else {
-            console.log("MySQL Keep-Alive Ping sent");
-          }
-    })
-}, 5 * 60 * 1000)
-
+  DB.query("SELECT 1", (err) => {
+    if (err) {
+      console.error("MySQL Keep-Alive Failed:", err.message);
+    } else {
+      console.log("MySQL Keep-Alive Ping sent");
+    }
+  });
+}, 5 * 60 * 1000);
 
 //Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
 
 //Endpoints
 app.use("/create-recruiter", createRecruiterRouter);
 app.use("/get-recruiter", getRecruiterRouter);
+app.use("/delete-all-recruiter", deleteAllRecruiterRouter);
+app.use("/delete-recruiter-by-id", deleteRecruiterByIdRouter);
 /////////////////////////////////////////////
 
-
 app.listen(Port, () => {
-    console.log(`Server is running on port ${Port}`);
-    DB.connect((err) => {
-        if(err) {
-            console.log("Error connecting to the database", err);
-        } else {
-            console.log("Connected to the database");
-        }
-    })
-})
+  console.log(`Server is running on port ${Port}`);
+  DB.connect((err) => {
+    if (err) {
+      console.log("Error connecting to the database", err);
+    } else {
+      console.log("Connected to the database");
+    }
+  });
+});
