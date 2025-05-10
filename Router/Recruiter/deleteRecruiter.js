5218621e88f85494f7a972dacf3180d98a70d8d1
moshaosama <mohamedOSFekry@gmail.com>
2025-05-10 23:44:02 +0300

    Latest Version

--- Router/Recruiter/deleteRecruiter.js
+++ Router/Recruiter/deleteRecruiter.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {
-  deleteRecruiterById,
   deleteAllRecruiter,
-} from "../../Controller/Recruiter/DeleteRecruiter.js";
+  deleteRecruiterById,
+} from "../../Controller/Recruiter/DeleteRecruiter";
 
-export const deleteAllRecruiterRouter = express.Router();
-export const deleteRecruiterByIdRouter = express.Router();
+const deleteAllRecruiterRouter = express.Router();
+const deleteRecruiterByIdRouter = express.Router();
 
 deleteAllRecruiterRouter.route("/").delete(deleteAllRecruiter);
 deleteRecruiterByIdRouter.route("/:id").delete(deleteRecruiterById);
+
+export { deleteAllRecruiterRouter, deleteRecruiterByIdRouter };
