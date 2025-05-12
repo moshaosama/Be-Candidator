24d9454598dbd48efcf2afa9af24f11465f742a6
moshaosama <mohamedOSFekry@gmail.com>
2025-05-12 21:37:40 +0300

    Add GetCompanies controller to fetch companies from database

---
+++ Controller/Company/GetCompanies.js
@@ -0,0 +1,19 @@
+import DB from "../../ConnectDB/DB.js";
+
+export const GetCompanies = async (req, res) => {
+  try {
+    const query = `SELECT * FROM Company`;
+    const [result] = await DB.promise().query(query);
+
+    return res.status(200).json({
+      success: true,
+      message: "Companies fetched successfully!",
+      result,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
