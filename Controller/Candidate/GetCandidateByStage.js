5c53b94afbafb9ada2534cb00444e92ffa7a4631
moshaosama <mohamedOSFekry@gmail.com>
2025-05-20 18:02:38 +0300

    Add: Get candidates by stage controller

--- Controller/Candidate/GetCandidateByStage.js
+++ Controller/Candidate/GetCandidateByStage.js
@@ -2,18 +2,31 @@ import DB from "../../ConnectDB/DB.js";
 
 export const GetCandidateByStage = async (req, res) => {
   try {
-    const { stageTitle } = req.params;
+    const { jobId, stageTitle } = req.params;
+
     if (!stageTitle) {
       return res.status(400).json({ message: "Stage title is required" });
     }
-    const GetCandidateByStage = "SELECT * FROM candidates WHERE Stages = ?";
-    const ValueGetCandidateByStage = [stageTitle];
-    const [result] = await DB.promise().query(
-      GetCandidateByStage,
-      ValueGetCandidateByStage
-    );
-    return res.status(200).json({ message: "Candidate by stage", result });
+
+    const query = "SELECT Candidates FROM job WHERE id = ?";
+    const values = [jobId];
+
+    const [result] = await DB.promise().query(query, values);
+
+    const candidates = JSON.parse(result[0].Candidates);
+    let candidatesByStage;
+
+    if (candidates) {
+      candidatesByStage = candidates.filter(
+        (candidate) => candidate.Stages === stageTitle
+      );
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Candidates by stage", result: candidatesByStage });
   } catch (error) {
+    console.error("Error in GetCandidateByStage:", error);
     return res.status(500).json({ message: "Internal server error", error });
   }
 };
