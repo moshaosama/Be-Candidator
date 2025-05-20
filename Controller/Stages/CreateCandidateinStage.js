cc7864b25d6c2cbe1f0f166c3156482e3e18c614
moshaosama <mohamedOSFekry@gmail.com>
2025-05-20 17:02:34 +0300

    Update CreateCandidateinStage: handle stage-wise candidate creation logic

--- Controller/Stages/CreateCandidateinStage.js
+++ Controller/Stages/CreateCandidateinStage.js
@@ -1,32 +1,46 @@
 import DB from "../../ConnectDB/DB.js";
 
 export const CreateCandidateinStage = async (req, res) => {
-  const { JobID } = req.body;
-  if (!JobID) {
-    return res.status(400).json({ message: "Job ID is required" });
+  try {
+    const { JobID } = req.body;
+    if (!JobID) {
+      return res.status(400).json({ message: "Job ID is required" });
+    }
+    const GetJobs = "SELECT * FROM job WHERE id = ?";
+    const Value = [JobID];
+
+    const [Job] = await DB.promise().query(GetJobs, Value);
+
+    const { CandidateID } = req.body;
+    if (!CandidateID) {
+      return res.status(400).json({ message: "Candidate ID is required" });
+    }
+
+    const UpdateCandidate = "UPDATE candidates SET Stages = ? WHERE id = ?";
+    const ValueUpdateCandidate = ["Pending", CandidateID];
+    await DB.promise().query(UpdateCandidate, ValueUpdateCandidate);
+
+    const GetCandidate = "SELECT * FROM candidates WHERE id = ?";
+    const ValueCandidate = [CandidateID];
+
+    const [Candidate] = await DB.promise().query(GetCandidate, ValueCandidate);
+
+    const GetCandidateinJob = "SELECT Candidates FROM job WHERE id = ?";
+    const ValueGetCandidateinJob = [JobID];
+    const [CandidateinJob] = await DB.promise().query(
+      GetCandidateinJob,
+      ValueGetCandidateinJob
+    );
+    const CurrentCandidateinJob = JSON.parse(CandidateinJob[0].Candidates);
+    CurrentCandidateinJob.push(Candidate[0]);
+
+    const UpdateJob = "UPDATE job SET Candidates = ? WHERE id = ?";
+    const ValueUpdateJob = [JSON.stringify(CurrentCandidateinJob), JobID];
+
+    const [result] = await DB.promise().query(UpdateJob, ValueUpdateJob);
+
+    return res.status(200).json({ message: "Candidate added to job", result });
+  } catch (err) {
+    return res.status(500).json({ message: "Internal server error", err });
   }
-  const GetJobs = "SELECT * FROM job WHERE id = ?";
-  const Value = [JobID];
-
-  const [Job] = await DB.promise().query(GetJobs, Value);
-  //   console.log(Job[0]);
-
-  const { CandidateID } = req.body;
-  if (!CandidateID) {
-    return res.status(400).json({ message: "Candidate ID is required" });
-  }
-
-  const GetCandidate = "SELECT * FROM candidates WHERE id = ?";
-  const ValueCandidate = [CandidateID];
-
-  const [Candidate] = await DB.promise().query(GetCandidate, ValueCandidate);
-
-  //   console.log(Candidate[0]);
-
-  const UpdateJob = "UPDATE job SET Candidates = ? WHERE id = ?";
-  const ValueUpdateJob = [JSON.stringify(Candidate[0]), JobID];
-
-  const [result] = await DB.promise().query(UpdateJob, ValueUpdateJob);
-
-  return res.status(200).json({ message: "Candidate added to job", result });
 };
