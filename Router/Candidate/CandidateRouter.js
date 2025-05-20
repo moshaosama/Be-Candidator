d044ea0eb985a572ec447edcbdc3dcc1762c3ad9
moshaosama <mohamedOSFekry@gmail.com>
2025-05-20 17:02:34 +0300

    Update CandidateRouter: add routing for stage-wise candidate operations

--- Router/Candidate/CandidateRouter.js
+++ Router/Candidate/CandidateRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createCandidate } from "../../Controller/Candidate/CreateCandidate.js";
 import { getCandidates } from "../../Controller/Candidate/GetCandidates.js";
-
+import { GetCandidateByStage } from "../../Controller/Candidate/GetCandidateByStage.js";
 const CandidateRouter = express.Router();
 
 CandidateRouter.route("/").post(createCandidate).get(getCandidates);
+CandidateRouter.route("/:stageTitle").get(GetCandidateByStage);
 
 export default CandidateRouter;
