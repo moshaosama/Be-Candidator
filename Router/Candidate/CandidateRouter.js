33b3231da62beacba022c417b57ab69f7708d558
moshaosama <mohamedOSFekry@gmail.com>
2025-05-27 21:23:58 +0300

    Update CandidateRouter routes

--- Router/Candidate/CandidateRouter.js
+++ Router/Candidate/CandidateRouter.js
@@ -2,9 +2,11 @@ import express from "express";
 import { createCandidate } from "../../Controller/Candidate/CreateCandidate.js";
 import { getCandidates } from "../../Controller/Candidate/GetCandidates.js";
 import { GetCandidateByStage } from "../../Controller/Candidate/GetCandidateByStage.js";
+import { getCandidateById } from "../../Controller/Candidate/getCandidateByid.js";
 const CandidateRouter = express.Router();
 
 CandidateRouter.route("/").post(createCandidate).get(getCandidates);
+CandidateRouter.route("/:candidateId").get(getCandidateById);
 CandidateRouter.route("/:jobId/:stageTitle").get(GetCandidateByStage);
 
 export default CandidateRouter;
