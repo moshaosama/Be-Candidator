376b8b62b41c6e50abc75f81997147ebb3b10e88
moshaosama <mohamedOSFekry@gmail.com>
2025-05-29 23:43:32 +0300

    Latest Version

--- Controller/EditProfile/EditDetailCandidate.js
+++ Controller/EditProfile/EditDetailCandidate.js
@@ -27,21 +27,34 @@ export const EditDetailsCandidates = async (req, res) => {
     const setClause = columns
       .map((col) => {
         const value =
-          data[col] !== undefined ? `'${data[col]}'` : `'${result[0]?.[col]}'`;
+          data[col] !== undefined ? `'${data[col]}'` : `'${result[0][col]}'`;
         return `${col} = ${value}`;
       })
       .join(", ");
 
     const QueryEditCandidate = `UPDATE candidates SET ${setClause} WHERE id = ?;`;
     const Values = [candidateId];
-
     await DB.promise().query(QueryEditCandidate, Values);
+
+    const ColumnsUser = columns.splice(0, 4);
+
+    const SetColumnsUser = ColumnsUser.map((col) => {
+      const values =
+        data[col] !== undefined ? `'${data[col]}'` : `'${result[0][col]}'`;
+
+      return `${col} = ${values}`;
+    });
+
+    const QueryEditUser = `UPDATE user SET ${SetColumnsUser} WHERE id = ?`;
+    await DB.promise().query(QueryEditUser, Values);
 
     return res.status(200).json({
       statusbar: "success",
       message: "Updated Successfully",
     });
   } catch (error) {
+    console.log(error);
+
     return res.status(500).json({
       success: false,
       message: "Internal Server Error",
