ae1200d34f096c2496e97d6a8925749cc65efd10
moshaosama <mohamedOSFekry@gmail.com>
2025-06-08 17:53:23 +0300

    modified: getApplyJob.js (Controller)

--- Controller/ApplyJob/getApplyJob.js
+++ Controller/ApplyJob/getApplyJob.js
@@ -1,5 +1,36 @@
+import DB from "../../ConnectDB/DB.js";
+
 export const getApplyJob = async (req, res) => {
   try {
+    const { candidator_id } = req.params;
+    if (!candidator_id) {
+      res.status(404).json({
+        statusbar: "error",
+        message: "Data is required",
+      });
+    }
+    const Query =
+      "SELECT * FROM  applyjob WHERE candidateId =? AND isApply= true";
+    const Value = [candidator_id];
+
+    const [result] = await DB.promise().query(Query, Value);
+
+    const QueryApply =
+      "SELECT * FROM applyjob AS A INNER JOIN job AS J ON  A.jobId = J.id WHERE isApply='true'";
+
+    const [resultApply] = await DB.promise().query(QueryApply);
+
+    if (resultApply.length == 0) {
+      res.status(404).json({
+        statusbar: "error",
+        message: "You don't have any Job",
+      });
+    }
+
+    res.status(200).json({
+      statusbar: "Success",
+      result: resultApply,
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
