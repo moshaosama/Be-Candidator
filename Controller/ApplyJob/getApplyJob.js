232673e132275e62db0d931eacd3fc8bb1d812dc
moshaosama <mohamedOSFekry@gmail.com>
2025-06-08 19:18:45 +0300

    modified: getApplyJob.js

--- Controller/ApplyJob/getApplyJob.js
+++ Controller/ApplyJob/getApplyJob.js
@@ -16,9 +16,11 @@ export const getApplyJob = async (req, res) => {
     const [result] = await DB.promise().query(Query, Value);
 
     const QueryApply =
-      "SELECT * FROM applyjob AS A INNER JOIN job AS J ON  A.jobId = J.id WHERE isApply='true'";
+      "SELECT * FROM applyjob AS A INNER JOIN job AS J ON  A.jobId = J.id WHERE isApply='true' And candidateId=?";
 
-    const [resultApply] = await DB.promise().query(QueryApply);
+    const ValueApply = [candidator_id];
+
+    const [resultApply] = await DB.promise().query(QueryApply, ValueApply);
 
     if (resultApply.length == 0) {
       res.status(404).json({
