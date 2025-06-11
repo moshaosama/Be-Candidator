013e910b52f3024c0bce0ccd0e5f57a09dda9586
moshaosama <mohamedOSFekry@gmail.com>
2025-06-11 18:49:30 +0300

    modified: SavedJobsRouter.js

--- Router/SavedJobs/SavedJobsRouter.js
+++ Router/SavedJobs/SavedJobsRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { CreateSavedJob } from "../../Controller/SavedJob/CreateSavedJob.js";
 import { GetJoinJobs } from "../../Controller/SavedJob/GetـJoinJobs.js";
+import { DeleteSavedJob } from "../../Controller/SavedJob/DeleteSavedJob.js";
 
 const SavedJobRouter = express.Router();
 
 SavedJobRouter.route("/").get(GetJoinJobs);
-SavedJobRouter.route("/:jobId").post(CreateSavedJob);
+SavedJobRouter.route("/:job_id").post(CreateSavedJob).delete(DeleteSavedJob);
 
 export default SavedJobRouter;
