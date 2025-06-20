2a230739ab0d01538483c2fed0d31090e887453b
moshaosama <mohamedOSFekry@gmail.com>
2025-06-20 19:41:43 +0300

    modified: updated ChangeUserProfile route handling

--- Router/ChangeProfile/ChangeUserProfile.js
+++ Router/ChangeProfile/ChangeUserProfile.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { ChanegUserProfile } from "../../Controller/ChangeProfile/ChangeUserProfile.js";
+import { ChangePassword } from "../../Controller/ChangeProfile/ChangePassword.js";
 
 const ChangeUserProfileRouter = express.Router();
+const ChangePasswordRouter = express.Router();
 
 ChangeUserProfileRouter.route("/:user_id").put(ChanegUserProfile);
+ChangePasswordRouter.route("/:user_id").put(ChangePassword);
 
-export default ChangeUserProfileRouter;
+export { ChangeUserProfileRouter, ChangePasswordRouter };
